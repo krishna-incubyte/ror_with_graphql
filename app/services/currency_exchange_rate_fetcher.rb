ab1dbb74599561d98f54fadcc61e4a447cb98524
Krishna <krishna@incubyte.co>
2026-07-20 10:43:35 +0530

    Remove dependency injection

--- app/services/currency_exchange_rate_fetcher.rb
+++ app/services/currency_exchange_rate_fetcher.rb
@@ -3,9 +3,9 @@ class CurrencyExchangeRateFetcher
   ERROR_CODE = 'error'
   BASE_URL = 'https://v6.exchangerate-api.com/v6'
 
-  def self.execute(source:, target:)
+  def self.execute(source:, target:, requester: HttpRequester)
     url = self.construct_url(source: source, target: target)
-    response = HttpRequester.new(url: url).make_get_request
+    response = requester.new(url: url).make_get_request
 
     result = response['result']
     if result == SUCCESS_CODE
